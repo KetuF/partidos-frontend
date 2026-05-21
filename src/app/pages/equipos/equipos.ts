c94fcfdfbe2030894e4165e01f448dc1dc6e4e5a
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2026-05-20 21:41:49 -0300

    new design equipo completo4

--- src/app/pages/equipos/equipos.ts
+++ src/app/pages/equipos/equipos.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EquiposService } from '../../services/equiposService';
 import { PartidosService } from '../../services/partidosService';
@@ -9,7 +9,6 @@ import { Fixture } from '../../components/fixture/fixture';
 import { Footer } from '../../components/footer/footer';
 import { AmonestacionesService } from '../../services/amonestacionesService';
 import { Amonestacion } from '../../interfaces/amonestaciones.interface';
-import { TablaAmonestaciones } from '../../components/tabla-amonestaciones/tabla-amonestaciones';
 
 @Component({
   selector: 'app-equipos',
@@ -19,6 +18,7 @@ import { TablaAmonestaciones } from '../../components/tabla-amonestaciones/tabla
   styleUrl: './equipos.css'
 })
 export class Equipos implements OnInit {
+
   private equiposService = inject(EquiposService);
   private partidosService = inject(PartidosService);
   private cdr = inject(ChangeDetectorRef);
@@ -28,111 +28,143 @@ export class Equipos implements OnInit {
   equipoSeleccionado: Equipo | null = null;
   partidosEquipo: Partido[] = [];
   amonestaciones: Amonestacion[] = [];
-  tabActiva: 'fixture' | 'amonestados' | 'plantilla' = 'fixture';
-  categoriaActualIndex = 0;
-  categorias: string[] = [];
+  tabActiva:'fixture'|'amonestados'|'plantilla'='fixture';
+  categoriaActualIndex=0;
 
-  plantilla = [
+  plantilla=[
     {
-      id: 1,
-      nombre: 'Juan Pérez',
-      dni: '41234567'
+      id:1,
+      nombre:'Juan Pérez',
+      dni:'41234567'
     },
     {
-      id: 2,
-      nombre: 'Lucas Gómez',
-      dni: '39876543'
+      id:2,
+      nombre:'Lucas Gómez',
+      dni:'39876543'
     }
   ];
 
-  ngOnInit(): void {
+  ngOnInit():void{
     this.equiposService.GetEquipos().subscribe({
-      next: (data) => {
-        this.equipos = data.sort((a, b) =>
-          a.nombre.localeCompare(b.nombre));
+
+      next:(data)=>{
+        this.equipos=data.sort(
+          (a,b)=>
+          a.nombre.localeCompare(b.nombre)
+        );
         this.cdr.detectChanges();
       },
-      error: (err) => console.error('Error al obtener equipos', err)
+      error:(err)=>
+      console.error(
+        'Error al obtener equipos',
+        err
+      )
     });
-    this.categorias = this.GetCategorias();
   }
 
-  SeleccionarEquipo(equipo: Equipo): void {
-    if (this.equipoSeleccionado?.id === equipo.id) {
-      this.equipoSeleccionado = null;
-      this.partidosEquipo = [];
+
+  SeleccionarEquipo(equipo:Equipo):void{
+
+    if(this.equipoSeleccionado?.id===equipo.id){
+      this.equipoSeleccionado=null;
+      this.partidosEquipo=[];
       return;
     }
-    this.equipoSeleccionado = equipo;
 
-    this.tabActiva = 'fixture';
+    this.equipoSeleccionado=equipo;
+    this.tabActiva='fixture';
 
-    this.partidosService.GetPartidosPorEquipo(equipo.id).subscribe({
-      next: (data) => {
-        this.partidosEquipo = data;
+    this.partidosService
+    .GetPartidosPorEquipo(equipo.id)
+    .subscribe({
+      next:(data)=>{
+        this.partidosEquipo=data;
         this.cdr.detectChanges();
-      },
-      error: (err) => console.error('Error al obtener partidos', err)
+      }
     });
 
-    this.amonestacionesService.GetAmonestaciones(equipo.id).subscribe({
-      next: (data) => {
-        this.amonestaciones = data;
+    this.amonestacionesService
+    .GetAmonestaciones(equipo.id)
+    .subscribe({
+      next:(data)=>{
+        this.amonestaciones=data;
         this.cdr.detectChanges();
-      },
-      error: (err) => console.error('Error al obtener amonestaciones', err)
+      }
     });
   }
 
-  GetCategorias(): string[] {
-    return [...new Set(this.equipos.map(e => e.categoria))];
+  GetCategorias():string[]{
+    return [
+      ...new Set(
+        this.equipos.map(
+          e=>e.categoria
+        )
+      )
+    ];
   }
 
-  GetEquiposPorCategoria(categoria: string): Equipo[] {
-    return this.equipos.filter(e => e.categoria === categoria);
-  }
+  CategoriaActual():string{
+    const categorias=
+    this.GetCategorias();
 
-  CambiarTab(tab: 'fixture' | 'amonestados' | 'plantilla') {
-    this.tabActiva = tab;
-  }
-
-  GetTarjetas(cantidad: number) {
-    return Array(cantidad);
-  }
-
-  CategoriaActual() {
-    return this.categorias[
+    if(categorias.length===0){
+      return '';
+    }
+    return categorias[
       this.categoriaActualIndex
     ];
   }
 
-  CategoriaSiguiente() {
 
-    if (this.categoriaActualIndex < this.categorias.length - 1) {
+
+  CategoriaSiguiente(){
+    const categorias=
+    this.GetCategorias();
+
+    if(this.categoriaActualIndex < categorias.length-1){
       this.categoriaActualIndex++;
-    } else {
-      this.categoriaActualIndex = 0;
+    }else{
+      this.categoriaActualIndex=0;
     }
     this.CerrarEquipo();
   }
 
-  CategoriaAnterior() {
-    if (
-      this.categoriaActualIndex > 0
-    ) {
-
+  CategoriaAnterior(){
+    const categorias=
+    this.GetCategorias();
+    if(
+      this.categoriaActualIndex>0
+    ){
       this.categoriaActualIndex--;
+    }
 
-    } else {
-
-      this.categoriaActualIndex =
-        this.categorias.length - 1;
-
+    else{
+      this.categoriaActualIndex=
+      categorias.length-1;
     }
     this.CerrarEquipo();
+  }
+
+
+
+  GetEquiposPorCategoria(categoria:string):Equipo[]{
+    return this.equipos.filter(
+      e=>e.categoria===categoria
+    );
+  }
+
+  CambiarTab(tab:'fixture'|'amonestados'|'plantilla'){
+    this.tabActiva=tab;
+  }
+
+  GetTarjetas(cantidad:number){
+    return Array(cantidad);
   }
 
   CerrarEquipo(){
     this.equipoSeleccionado=null;
+    this.partidosEquipo=[];
+    this.amonestaciones=[];
   }
+
 }
