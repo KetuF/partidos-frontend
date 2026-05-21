8fe4f991dcf11644afc888078f45e8d8128dcd89
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2026-05-21 18:55:19 -0300

    add foto/escudo en fixture front

--- src/app/interfaces/partido.interface.ts
+++ src/app/interfaces/partido.interface.ts
@@ -4,8 +4,13 @@ export interface Partido {
   hora: string;
   jugado: number;
   numero_fecha: number;
+
   goles_local: number;
   goles_visitante: number;
+
   equipo_local: string;
   equipo_visitante: string;
+
+  escudo_local:string;
+  escudo_visitante:string;
 }
